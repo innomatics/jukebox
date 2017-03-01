4cf8568a4fb789d6db1b5dfe313e8e4b42358c15
Justin Hopkins <justin@commoncode.com.au>
2017-03-01 23:36:44 +1100

    Use local storage. Parsing tags using https://github.com/aadsm/jsmediatags

--- manage.js
+++ manage.js
@@ -1,14 +1,60 @@
+var songList = [];
+var songFolder = '';
+var jsmediatags = window.jsmediatags;
+
+function printSongList()
+{
+  var result = '';
+  for (var i = 0; i < songList.length; i++)
+  {
+    result += songList[i].title + ' by ' + songList[i].artist + '<BR>'
+  }
+  document.getElementById('songList').innerHTML = result;
+}
+
+function addSong(artist, title, cover)
+{
+  var newSong = {};
+  newSong.artist = artist;
+  newSong.title = title;
+  newSong.cover = cover;
+  songList.push(newSong);
+  printSongList();
+  localStorage.setItem('songList', JSON.stringify(songList));
+}
+
 function addFile(file)
 {
   if (file)
   {
-    var reader = new FileReader();
-    reader.onload = function ()
+
+    jsmediatags.read(file,
     {
-      alert(this.result);
-    }
-    reader.readAsDataURL(file);
-    alert(file.name);
+      onSuccess: function (tag)
+      {
+        console.log(tag);
+        var artist = 'unknown';
+        var title = file.name;
+        var cover = null;
+        if (tag.tags.hasOwnProperty('artist'))
+        {
+          artist = tag.tags.artist;
+        }
+        if (tag.tags.hasOwnProperty('title'))
+        {
+          title = tag.tags.title;
+        }
+        if (tag.tags.hasOwnProperty('picture'))
+        {
+          cover = tag.tags.picture;
+        }
+        addSong(artist, title, cover);
+      },
+      onError: function (error)
+      {
+        console.log(error);
+      }
+    });
 
     // Add file to local JSON
   }
@@ -16,21 +62,42 @@ function addFile(file)
 
 function addFiles(e)
 {
-  var files = e.target.files;
-  for (var i=0; i<files.length; i++)
+  if (songFolder.length == 0)
   {
-    addFile(files[i]);
+    alert('Make sure to set the song folder first.');
   }
+  else
+  {
+    songList = [];
+    var files = e.target.files;
+    for (var i = 0; i < files.length; i++)
+    {
+      addFile(files[i]);
+    }
+  }
+}
+
+function setFolder(e)
+{
+  songFolder = e.target.value;
 }
 
 function AddListeners()
 {
   document.getElementById('addFiles').addEventListener('change', addFiles, false);
+  document.getElementById('songFolder').addEventListener('change', setFolder, false);
 }
 
 function appStart()
 {
   AddListeners();
+  songFolder = document.getElementById('songFolder').value;
+  songList = JSON.parse(localStorage.getItem('songList'));
+  if (!songList)
+  {
+    songList = [];
+  }
+  printSongList();
 }
 
 (function ()
--- play.js
+++ play.js
@@ -1,11 +1,34 @@
-function drawUI() {
+var songList = [];
+
+function printSongList()
+{
+  songList = JSON.parse(localStorage.getItem('songList'));
+  if (!songList)
+  {
+    alert('No songs loaded!!');
+  }
+  else
+  {
+    var result = '';
+    for (var i = 0; i < songList.length; i++)
+    {
+      result += songList[i].title + ' by ' + songList[i].artist + '<BR>'
+    }
+    document.getElementById('songList').innerHTML = result;
+  }
+}
+
+function drawUI()
+{
   // Read JSON from local storage
+  printSongList();
 
   // Build audio html5 elements
 }
+
 function AddListeners()
 {
-  document.getElementById('addFiles').addEventListener('change', addFiles, false);
+  //document.getElementById('addFiles').addEventListener('change', addFiles, false);
 }
 
 function appStart()
