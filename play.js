40425d155f07068ac7f4566344037a7e2d126678
Justin Hopkins <justin@commoncode.com.au>
2017-03-02 23:21:29 +1100

    Proper buttons

--- play.js
+++ play.js
@@ -1,15 +1,74 @@
 var songList = [];
 var songFolder = '';
+var two32 = 4294967295; // 2^32
+var nowPlaying = '';
 
-function drawPlayButton(parentElement, filename, artist, title, cover)
+String.prototype.hashCode = function ()
+{
+  var hash = 0;
+  if (this.length == 0) return hash;
+  for (i = 0; i < this.length; i++)
+  {
+    var char = this.charCodeAt(i);
+    hash = ((hash << 5) - hash) + char;
+    hash = hash & hash; // Convert to 32bit integer
+    hash += two32;
+  }
+  return hash;
+}
+
+function getRandomColor(title)
+{
+  var bigint = title.hashCode();
+  var hex = bigint.toString(16);
+  var smallhex = parseInt(hex, 16) % parseInt('0xffffff', 16);
+  return '#' + smallhex.toString(16);
+}
+
+function songclick(e)
+{
+  var id = e.target.id;
+  if (id == '')
+  {
+    id = e.target.parentElement.id;
+  }
+  id = id.replace(/\D/g, '');
+  var audio = document.getElementById('audio' + id);
+  var btn = document.getElementById('button' + id);
+
+  if (nowPlaying == id)
+  {
+    audio.pause();
+    nowPlaying = '';
+    btn.classList.remove('nowPlaying');
+  }
+  else if (nowPlaying == '')
+  {
+    audio.play();
+    nowPlaying = id;
+    btn.classList.add('nowPlaying');
+  }
+}
+
+function drawPlayButton(id, parentElement, filename, artist, title, cover)
 {
   var sound = document.createElement('audio');
-  sound.id = 'audio-player';
-  sound.controls = 'controls';
+  sound.id = 'audio' + id;
+  //sound.controls = 'controls';
   sound.src = 'file://' + songFolder + filename;
   sound.type = 'audio/mpeg';
   parentElement.appendChild(sound);
 
+  var btn = document.createElement('div');
+  btn.id = 'button' + id;
+  btn.innerHTML = '<span>' + artist + '<BR>' + title + '</span>';
+  btn.addEventListener('click', function (e)
+  {
+    songclick(e);
+  }, false);
+  btn.classList.add('playButton');
+  btn.style.backgroundColor = getRandomColor(title);
+  parentElement.appendChild(btn);
 }
 
 function printSongList()
@@ -25,15 +84,12 @@ function printSongList()
     var result = '';
     for (var i = 0; i < songList.length; i++)
     {
-      drawPlayButton(buttons,
+      drawPlayButton(i, buttons,
         songList[i].filename,
         songList[i].artist,
         songList[i].title,
         songList[i].cover);
-
-      result += songList[i].title + ' by ' + songList[i].artist + '<BR>'
     }
-    document.getElementById('songList').innerHTML = result;
   }
 }
 
