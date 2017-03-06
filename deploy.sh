0e9208faa9f101397a224bb11f64dd9f7108bd8a
Justin Hopkins <justin@commoncode.com.au>
2017-03-06 14:07:46 +1100

    Added spinner to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source deploy.config
-scp -P $SCP_PORT deploy.sh index.html jsmediatags.min.js manage.js play.js styles.css $SCP_SERVER_DIR
+scp -P $SCP_PORT deploy.sh index.html jsmediatags.min.js manage.js play.js styles.css spinner.gif $SCP_SERVER_DIR
